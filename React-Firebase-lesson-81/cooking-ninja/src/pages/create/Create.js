205dd3fce5cb8607086f413f05b3da9fe6b54987
aaliyahraquel <liyah_r_j@live.co.uk>
2024-11-04 23:16:09 +0000

    add ingredients functionality

--- React-Firebase-lesson-81/cooking-ninja/src/pages/create/Create.js
+++ React-Firebase-lesson-81/cooking-ninja/src/pages/create/Create.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 // styles
 import './Create.css'
@@ -7,12 +7,27 @@ export default function Create() {
   const [title, setTitle] = useState('')
   const [method, setMethod] = useState('')
   const [cookingTime, setCookingTime] = useState('')
+  const [newIngredient, setNewIngredient] = useState('')
+  const [ingredients, setIngredients] = useState([])
+  const ingredientsInput = useRef(null)
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(title, method, cookingTime)
+    console.log(title, method, cookingTime, ingredients)
   }
+  
+  const handleAdd = (e) => {
+    e.preventDefault()
+    const ing = newIngredient.trim()
 
+    // prevent duplicates 
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients((prevIngredients) => [...prevIngredients, ing])
+    }
+    setNewIngredient('')
+    //focus an input field using refs
+    ingredientsInput.current.focus()
+  }
   return (
     <div className="create">
       <h2 className="page-title">Add a New Recipe</h2>
@@ -27,7 +42,19 @@ export default function Create() {
           />
         </label>
 
-        {/* ingredients go here */}
+        <label>
+          <span>Recipe ingredients</span>
+          <div className="ingredients">
+            <input 
+              type="text"
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientsInput}
+            />
+            <button onClick={handleAdd} className="btn">Add</button>
+          </div>
+        </label>
+        <p>Current ingredients: {ingredients.map(i => <em key={i}>{i}, </em>)}</p>
 
         <label>
           <span>Recipe Method:</span>
